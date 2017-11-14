00508ce31cc95d4523c510aebd4a9cb055a94565
Bob Cousins <bobcousins42@googlemail.com>
2017-11-14 17:32:00 +0000

    Update cmos4000

---
+++ symgen/data/cmos4000/tdump.bat
@@ -0,0 +1,7 @@
+
+rem dump a library to symgen format
+
+rem python ..\..\..\sym_gen.py -d --lib C:\git_bobc\kicad-library\library\74xx.lib --ref ..\7400_logic_ref.txt
+
+python ..\..\sym_gen.py -d --lib C:\git_bobc\kicad-library\library\cmos4000.lib --ref 4000_list.csv
+
---
+++ symgen/data/cmos4000/tgen.bat
@@ -0,0 +1,6 @@
+
+rem generate a library
+
+
+python ..\..\sym_gen.py --inp cmos4000.txt
+
